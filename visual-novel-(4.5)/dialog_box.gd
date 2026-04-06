6575ece281163964fa5947a7f6ca109535993c6a
Anton <Antonjakobsenarbejde@gmail.com>
2026-04-06 15:07:55 +0200

    Design til settings
    Designopdateringer
    Problemer med skalering på y-aksen

---
+++ visual-novel-(4.5)/dialog_box.gd
@@ -0,0 +1,63 @@
+class_name DialogBox extends Panel
+
+var flick:bool=false
+
+@export var dialog:String="lorem ipsum...":
+	set(n):
+		dialog=n
+		reset_text()
+var tween:Tween
+
+signal complete
+
+func _on_button_pressed() -> void:
+	flick=true
+	print("click button ")
+
+func _on_button_button_up() -> void:
+	await get_tree().create_timer(1.0).timeout
+	flick=false
+
+func _input(event: InputEvent) -> void:
+	await get_tree().create_timer(0.2).timeout
+	if ! is_node_ready():
+		return
+	if event is InputEventMouseButton and event.is_pressed() and flick==true:
+		print ("Click")
+		advance()
+
+func advance():
+	if tween:
+		print("Skipping ahead")
+		tween.stop()
+		tween=null
+		%Dialogbox.visible_ratio=1
+	else:
+		print("Killing")
+		self_destruct()
+
+func self_destruct():
+	get_parent().remove_child(self)
+	complete.emit()
+	queue_free()
+
+func _ready() -> void:
+	reset_text()
+
+func reset_text():
+	if ! is_node_ready():
+		return
+	%Dialogbox.text=dialog
+	%Dialogbox.visible_ratio=0
+	if tween:
+		tween.stop()
+	tween = get_tree().create_tween()
+	@warning_ignore("integer_division")
+	tween.tween_property(%Dialogbox, "visible_ratio", 1, (dialog.length()/30) +0.5).set_trans(Tween.TRANS_LINEAR)
+	await tween.finished
+	tween=null
+	#self_destruct() ???? måske
+
+func _on_texture_button_pressed():
+	print("Manual skip")
+	advance()
---
+++ visual-novel-(4.5)/main_scene.gd
@@ -0,0 +1,27 @@
+class_name SetDressing extends Control
+
+const CHARA1 = preload("res://character_example.tscn")
+
+func _ready() -> void:
+	var o:VNCharacter=CHARA1.instantiate()
+	add_child(o)
+	
+	o.snap(%right)
+	await o.move(%far_right,0.5)
+	await wait(0.25)
+	await o.move(%right,0.5)
+	await wait(0.25)
+	o.flip_h=true
+	await o.say("hola")
+	await o.move(%leftish,1)
+	await wait(0.5)
+	await o.say("halloo!")
+	await o.move(%rightish,2)
+	await wait(1)
+	await o.move(%caCenter,2)
+
+func wait(seconds:float) -> Signal:
+	return get_tree().create_timer(seconds).timeout
+
+func _on_settings_button_down() -> void:
+	get_tree().change_scene_to_file("res://options.tscn")
