cd03a4e5d7eb86d89fe0d840e2cfabcb865d1bb1
Anton <Antonjakobsenarbejde@gmail.com>
2026-04-08 14:52:23 +0200

    +1 character

--- visual-novel/main_scene.gd
+++ visual-novel/main_scene.gd
@@ -1,19 +1,24 @@
 class_name SetDressing extends Control
 
 const CHARA1 = preload("res://character_example.tscn")
+const CHARA2 = preload("res://character_example2.tscn")
 var overlay = preload("res://options.tscn").instantiate()
 func _ready() -> void:
 	var o:VNCharacter=CHARA1.instantiate()
+	var b:VNCharacter=CHARA2.instantiate()
 	add_child(o)
+	add_child(b)
 	
+	b.snap(%left)
 	o.snap(%right)
 	await o.move(%far_right,0.5)
 	await wait(0.25)
-	await o.move(%right,0.5)
+	o.move(%leftish,0.5)
+	await b.move(%right,0.5)
 	await wait(0.25)
 	o.flip_h=true
 	await o.say("test","Last year I would still have been in middle school.\n  It’s been a while since then, we didn’t know each \n  other exactly.")
-	await o.move(%leftish,1)
+	await o.move(%right,1)
 	await wait(0.5)
 	await o.say("test","lorem ipsum 2")
 	await o.move(%rightish,1)
